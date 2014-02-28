bc623ba440d05146a2db4a48b7f7a438ce1acea9
slmnn <arto.salminen@gmail.com>
2014-02-28 15:06:20 +0200

    Moving some parameters to myconf.js

--- config/adapters.js
+++ config/adapters.js
@@ -28,14 +28,3 @@ module.exports.adapters = {
     url: process.env.DB_URL,
   }
 };
-
-module.exports = {
-  sc_salt : process.env.SC_SALT,
-  sc_password : process.env.SC_PASSWORD,
-
-  default_territory_holder : 'Aluepöytä',
-  default_territory_holder_id : 1,
-  admin_init_password : '2348jhj3hh',
-
-  territory_letters : ['A','K','L','M'],
-};
---
+++ config/myconf.js
@@ -0,0 +1,10 @@
+module.exports = {
+  sc_salt : process.env.SC_SALT,
+  sc_password : process.env.SC_PASSWORD,
+
+  default_territory_holder : 'Aluepöytä',
+  default_territory_holder_id : 1,
+  admin_init_password : '2348jhj3hh',
+
+  territory_letters : ['A','K','L','M'],
+};
