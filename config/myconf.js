a4a9795356042c62de8e4aca93276f554666d16f
slmnn <arto.salminen@gmail.com>
2014-03-01 14:58:29 +0200

    Not covered limit parametrized

--- config/myconf.js
+++ config/myconf.js
@@ -6,5 +6,6 @@ module.exports = {
   default_territory_holder_id : '5310a361156abf701b063329',
   admin_init_password : '2348jhj3hh',
 
+  limit_for_rarely_covered_territory : 30, // days
   territory_letters : ['A','K','L','M']
 };
