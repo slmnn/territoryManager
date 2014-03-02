a50c107c0118f82aab60948ed82658bb55c37f81
slmnn <arto.salminen@gmail.com>
2014-03-02 10:46:14 +0200

    s13

--- api/controllers/TerritoryController.js
+++ api/controllers/TerritoryController.js
@@ -415,7 +415,8 @@ module.exports = {
                   t_with_holder_names[i].holderHistory[k][0] = h[j].name;
                   t_with_holder_names[i].holderHistoryWithNames.push([h[j].name, t_with_holder_names[i].holderHistory[k][1]]);
                 } else if( j == h.length - 1) {
-                  t_with_holder_names[i].holderHistoryWithNames.push('Removed holder', t_with_holder_names[i].holderHistory[k][1]]);
+                  t_with_holder_names[i].holderHistory[k][0] = 'Removed holder';
+                  t_with_holder_names[i].holderHistoryWithNames.push(['Removed holder', t_with_holder_names[i].holderHistory[k][1]]);
                 }
               }
             }
