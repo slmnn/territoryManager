e959682c49b6f0167418ff47409a50370fb29252
slmnn <arto.salminen@gmail.com>
2014-03-11 15:26:16 +0200

    Styling, Finnish localization

--- config/myconf.js
+++ config/myconf.js
@@ -7,5 +7,5 @@ module.exports = {
   admin_init_password : '2348jhj3hh',
 
   limit_for_rarely_covered_territory : process.env.LIMIT_RARELY_COVERED, // days
-  territory_letters : ['A','K','L','M']
+  territory_letters : ['A','H','K','L','M','N','V']
 };
