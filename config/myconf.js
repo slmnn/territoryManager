6e77fbb1aef3b483dfa2c010e919ca30735c8019
Slmnn <arto.salminen@slmnn.com>
2015-05-17 09:36:01 +0300

    Parametrisoitu oletusomistajan tiedot

--- config/myconf.js
+++ config/myconf.js
@@ -2,8 +2,8 @@ module.exports = {
   sc_salt : process.env.SC_SALT,
   sc_password : process.env.SC_PASSWORD,
 
-  default_territory_holder : 'Aluepöytä',
-  default_territory_holder_id : '5310a361156abf701b063329',
+  default_territory_holder : process.env.DEFAULT_HOLDER_NAME || 'Aluepöytä',
+  default_territory_holder_id : process.env.DEFAULT_HOLDER_ID || '5310a361156abf701b063329',
   admin_init_password : '2348jhj3hh',
 
   limit_for_rarely_covered_territory : process.env.LIMIT_RARELY_COVERED || 365, // days
